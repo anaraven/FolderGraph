bb68bac4b6c83fb0bad653c063d422dfdc2c54ea
Andres Aravena <andres.aravena@gmail.com>
2015-12-10 21:19:10 +0200

    working version, graph based

--- foldergraph.py
+++ foldergraph.py
@@ -1,25 +1,50 @@
 #!/usr/bin/env python
+# vim: set fileencoding=utf8 :
+
 import igraph, os
 
 filename = "/Users/anaraven/Desktop/mybookfiles.txt"
 
-tree = {}
+g = igraph.Graph()
+
+def add_node(graph, node):
+    try:
+        graph.vs.find(node)
+    except ValueError as e:
+        graph.add_vertex(node)
+
 def join_nodes(nodes):
     for i in range(len(nodes)-1):
-        src = tree.get(nodes[i], {})
+        src = nodes[i]
+        add_node(g, src)
         for j in range(i+1, len(nodes)):
-            src[nodes[j]] = src.get(nodes[j], 0) + 1
+            dst = nodes[j]
+            add_node(g, dst)
+            g.add_edge(src, dst, weight=1)
 
 nodes = []
+NR = 0
+tot_size = 0
 for line in file(filename, "r"):
     if line.startswith("#"):
         continue
+    if line.find("Public/Shared Videos/Música")>=0:
+        continue
     duptype, iden, depth, size, device, inode, priority, name = line.strip().split(" ", 7)
+    if int(size)<10000000:
+        continue
     dirn = os.path.dirname(name)
     if duptype == "DUPTYPE_FIRST_OCCURRENCE":
+        NR += 1
         join_nodes(nodes)
-        nodes = [name]
+        nodes = [dirn]
+        if NR % 100==0:
+            print NR
     else:
-        nodes.append(name)
+        tot_size += int(size)
+        nodes.append(dirn)
 
-print len(tree.keys())
+g.simplify(combine_edges=sum)
+print g.summary()
+print g.components()
+print int(tot_size/1024/1024/1024),"G"
