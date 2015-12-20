6cee8bcc5fe3fa915e57652251ad9eb60efdce8b
Andres Aravena <andres.aravena@gmail.com>
2015-12-20 13:06:03 +0200

    if node is repeated, increase count and size. Contract with our own function. Define `printout()` function

--- foldergraph.py
+++ foldergraph.py
@@ -10,9 +10,11 @@ ignore_fname = "ignore.txt"
 
 g = igraph.Graph(directed=False)
 
-def add_node(graph, node, nd=0, nf=0):
+def add_node(graph, node, nd, nf):
     try:
-        graph.vs.find(node)
+        v = graph.vs.find(node)
+        v["nd"] += nd
+        v["nf"] += nf
     except ValueError as e:
         graph.add_vertex(name=node, nd=nd, nf=nf)
 
@@ -49,13 +51,18 @@ def add_pct(g, thr, out = []):
         count = edge["count"]
         nf0 = g.vs["nf"][e0]
         nf1 = g.vs["nf"][e1]
-        pct0 = int(count*1000/nf0)/10
-        pct1 = int(count*1000/nf1)/10
+        try:
+            pct0 = int(count*1000/nf0)/10
+        except ZeroDivisionError:
+            print g.vs["name"][e0], nf0, nf1
+            pct0 = 0
+        try:
+            pct1 = int(count*1000/nf1)/10
+        except ZeroDivisionError:
+            print g.vs["name"][e0], nf0, nf1
+            pct1 = 0
         pct = max(pct0, pct1)
         if pct >= thr:
-#            print( "%5.1f\n%s\t%d\t%d\n%s\t%d\t%d\n" % (pct,
-#                              g.vs["name"][e0], nf0, pct0,
-#                              g.vs["name"][e1], nf1, pct1) )
             out.append( (pct, g.vs["name"][e0], nf0, pct0,
                               g.vs["name"][e1], nf1, pct1) )
     return out
@@ -69,48 +76,38 @@ for line in file(dup_filename, "r"):
     if should_ignore(line):
         continue
     duptype, iden, depth, size, device, inode, priority, name = line.strip().split(" ", 7)
-    if int(size)<1000000:
+    if int(size)<100000000:
         continue
     dirn = os.path.dirname(name)
     if duptype == "DUPTYPE_FIRST_OCCURRENCE":
-#         NR += 1
         join_nodes(nodes)
         nodes = [ (dirn, int(size)) ]
-#        if NR % 100==0:
-#            print NR
     else:
         tot_size += int(size)
         nodes.append( (dirn, int(size)) )
 
-g.simplify(combine_edges=sum)
+g.simplify(combine_edges="sum")
 print g.summary()
-out = add_pct(g, 80)
+out = add_pct(g, 95)
 
-def gen_tuple(e0, e1):
-    try:
-        eid = g.get_eid(e0, e1)
-    except igraph._igraph.InternalError as e:
-        eid = g.get_eid(e1, e0)
-    count = g.es["count"][eid]
-    nf0 = g.vs["nf"][e0]
-    nf1 = g.vs["nf"][e1]
-    pct0 = int(count*1000/nf0)/10
-    pct1 = int(count*1000/nf1)/10
-    pct = max(pct0, pct1)
-    return (pct, g.vs["name"][e0], nf0, pct0, g.vs["name"][e1], nf1, pct1)
+def contract(g):
+    ng = igraph.Graph(directed=False)
+    for vertex in g.vs:
+        add_node(ng, os.path.dirname(vertex["name"]),
+                 nd=vertex["nd"], nf=vertex["nf"])
+    print ng.summary()
+    for edge in g.es:
+        e0, e1 = edge.tuple
+        ng.add_edge(os.path.dirname(g.vs["name"][e0]),
+                    os.path.dirname(g.vs["name"][e1]),
+                    weight=edge["weight"], count=edge["count"])
+    print ng.summary()
+    ng.simplify(combine_edges="sum")
+    return ng
 
-#comps = g.components()
-# for el in comps:
-#     if len(el)==1:
-#         continue
-#     for i in range(len(el)-1):
-#         for j in range(i+1, len(el)):
-#             try:
-#                 o = gen_tuple(el[i], el[j])
-#             except igraph._igraph.InternalError as e:
-#                 continue
-#             if o[0]>=80.0:
-#                 out.append( o )
+g = contract(g)
+print g.summary()
+out2 = add_pct(g, 95)
 
 def comp_tuple(x,y):
     if(x[0]<y[0]):
@@ -119,10 +116,15 @@ def comp_tuple(x,y):
         return 0
     return 1
 
-out.sort(comp_tuple)
+def printout(out):
+    out.sort(comp_tuple)
+    for o in out:
+        print o[1], o[2], o[3]
+        print o[4], o[5], o[6], "\n"
+    print len(out), int(tot_size/1024/1024/1024),"G"
 
-for o in out:
-    print o[1], o[2], o[3]
-    print o[4], o[5], o[6], "\n"
+#            print( "%5.1f\n%s\t%d\t%d\n%s\t%d\t%d\n" % (pct,
+#                              g.vs["name"][e0], nf0, pct0,
+#                              g.vs["name"][e1], nf1, pct1) )
 
-print len(out), int(tot_size/1024/1024/1024),"G"
+printout(out2)
